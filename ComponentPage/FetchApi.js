6e7cf8382fa03ebc18a22fec67140d4992d35c65
haiquyhq123 <haiquyhq123@gmail.com>
2025-01-21 11:56:19 -0500

    Complete random button

--- ComponentPage/FetchApi.js
+++ ComponentPage/FetchApi.js
@@ -19,13 +19,50 @@ async function TakeApi(url) {
         console.error(er);
     }
 }
+let countChange = 0;
 TakeApi("https://restcountries.com/v3.1/all");
 let imgArea = document.querySelector(".img-are");
 let randomButton = document.querySelector(".random");
+let randomValue = document.querySelectorAll(".radio-wrapper");
 randomButton.addEventListener("click",(event)=>{
     let ranChoice = Math.round(Math.random()*251);
     let country = arrayOfCountry[ranChoice];
     let imgTag = document.querySelector(".img-tag");
     imgTag.setAttribute("src",country.img);
-    
-})
+    let ranChoiceCorret = Math.round(Math.random()*3);
+    let firstRanChoice = Math.round(Math.random()*250);
+    let secondRanChoice = Math.round(Math.random()*250);
+    let thirdRanChoice = Math.round(Math.random()*250);
+    console.log(ranChoiceCorret);
+    if(ranChoiceCorret == 0){
+        // 1,2,3
+        randomValue[ranChoiceCorret].lastElementChild.innerText = country.name;
+        randomValue[1].lastElementChild.innerText = arrayOfCountry[firstRanChoice].name;
+        randomValue[2].lastElementChild.innerText = arrayOfCountry[secondRanChoice].name;
+        randomValue[3].lastElementChild.innerText = arrayOfCountry[thirdRanChoice].name;
+    }
+    else if(ranChoiceCorret == 1){
+        // 0,2,3
+        randomValue[ranChoiceCorret].lastElementChild.innerText = country.name;
+        randomValue[0].lastElementChild.innerText = arrayOfCountry[firstRanChoice].name;
+        randomValue[2].lastElementChild.innerText = arrayOfCountry[secondRanChoice].name;
+        randomValue[3].lastElementChild.innerText = arrayOfCountry[thirdRanChoice].name;
+    }
+    else if(ranChoiceCorret == 2){
+        //0,1,3
+        randomValue[ranChoiceCorret].lastElementChild.innerText = country.name;
+        randomValue[0].lastElementChild.innerText = arrayOfCountry[firstRanChoice].name;
+        randomValue[1].lastElementChild.innerText = arrayOfCountry[secondRanChoice].name;
+        randomValue[3].lastElementChild.innerText = arrayOfCountry[thirdRanChoice].name;
+    }
+    else if(ranChoiceCorret == 3){
+        // 0, 1, 2
+        randomValue[ranChoiceCorret].lastElementChild.innerText = country.name;
+        randomValue[0].lastElementChild.innerText = arrayOfCountry[firstRanChoice].name;
+        randomValue[1].lastElementChild.innerText = arrayOfCountry[secondRanChoice].name;
+        randomValue[2].lastElementChild.innerText = arrayOfCountry[thirdRanChoice].name;
+    }
+    console.log(country.name);
+    console.log(randomValue[ranChoiceCorret].lastElementChild.innerText);
+
+});
